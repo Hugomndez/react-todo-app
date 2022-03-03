aaf0fe444e5df4b2d255655a039e3683d376e0f8
Hugomndez <hello@hugomendez.dev>
2022-03-02 23:06:58 -0600

    Added css to EmptyTask

--- src/components/EmptyTask/EmptyTask.tsx
+++ src/components/EmptyTask/EmptyTask.tsx
@@ -1,5 +1,7 @@
+import './EmptyTask.css';
+
 const EmptyTask = () => {
-  return <p>Create your first To Do!</p>;
+  return <p className="EmptyTask">Add your first To Do!</p>;
 };
 
 export default EmptyTask;
